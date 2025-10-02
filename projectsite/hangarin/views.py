f9ed4f9404724cdc4586779fd092d3311d2aa8c6
arvi <yurirago@gmail.com>
2025-10-02 18:04:13 +0800

    bootstrap 5 mantis django static-03

--- projectsite/hangarin/views.py
+++ projectsite/hangarin/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from django.views.generic.list import ListView
+from hangarin.models import Task
 
 # Create your views here.
+class HomePageView(ListView):
+    model = Task
+    context_object_name = 'home'
+    template_name = "home.html"
